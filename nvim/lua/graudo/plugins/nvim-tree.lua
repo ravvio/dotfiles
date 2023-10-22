48f514d21cd1e3aa3f510a0b224be6838718657f
Alessio Raviola <alessio.raviola.98@gmail.com>
2023-10-22 20:11:12 +0200

    Small edits to tmux and nvim

--- nvim/lua/graudo/plugins/nvim-tree.lua
+++ nvim/lua/graudo/plugins/nvim-tree.lua
@@ -12,7 +12,7 @@ local function my_on_attach(bufnr)
     api.config.mappings.default_on_attach(bufnr)
 
     -- custom mappings
-    vim.keymap.set('n', '<C-->', api.tree.change_root_to_node, opts('CD'))
+    vim.keymap.set('n', '_', api.tree.change_root_to_node, opts('CD'))
     vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
 end
 
