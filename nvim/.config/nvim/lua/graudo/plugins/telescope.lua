418f4ced195335beebf5d62df2e3256f19d48603
Alessio Raviola <alessio.raviola@codeploy.it>
2023-11-20 15:25:48 +0100

    Add bindings for telescope and lsp

--- nvim/.config/nvim/lua/graudo/plugins/telescope.lua
+++ nvim/.config/nvim/lua/graudo/plugins/telescope.lua
@@ -7,5 +7,9 @@ return {
         vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+
+        vim.keymap.set("n", "<leader>fe", builtin.diagnostics, {})
+        vim.keymap.set("n", "<leader>fi", builtin.lsp_implementations, {})
+        vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
     end,
 }
