ddcd6a695298936415daaab440edb12aa7f2a101
Alessio Raviola <alessio.raviola@codeploy.it>
2023-12-06 14:55:52 +0100

    terminal improvements

--- p10k/.p10k.zsh
+++ p10k/.p10k.zsh
@@ -94,6 +94,8 @@
   # Prompt symbol in overwrite vi mode is the same as in command mode.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_OVERWRITE_STATE=false
 
+  typeset -g ZLE_PROMPT_INDENT=1
+
   # Grey Python Virtual Environment.
   typeset -g POWERLEVEL9K_VIRTUALENV_FOREGROUND=$grey
   # Don't show Python version.
