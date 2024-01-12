ae0c457e5332ef6e1f719fc125ec5eb2efc4e185
Alessio Raviola <alessio.raviola@codeploy.it>
2024-01-12 11:45:42 +0100

    new harpoon bindings

--- nvim/.config/nvim/lua/graudo/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/graudo/plugins/harpoon.lua
@@ -10,11 +10,9 @@ return {
 
         vim.keymap.set('n', '<Leader>-', function() harpoon_ui.toggle_quick_menu() end)
 
-        vim.keymap.set('n', '<Leader>o1', function() harpoon_ui.nav_file(1) end)
-        vim.keymap.set('n', '<Leader>o2', function() harpoon_ui.nav_file(2) end)
-        vim.keymap.set('n', '<Leader>o2', function() harpoon_ui.nav_file(3) end)
-        vim.keymap.set('n', '<Leader>o3', function() harpoon_ui.nav_file(4) end)
-        vim.keymap.set('n', '<Leader>o4', function() harpoon_ui.nav_file(5) end)
+        vim.keymap.set('n', '<C-b>', function() harpoon_ui.nav_file(1) end)
+        vim.keymap.set('n', '<C-n>', function() harpoon_ui.nav_file(2) end)
+        vim.keymap.set('n', '<C-m>', function() harpoon_ui.nav_file(3) end)
 
         vim.keymap.set('n', '<C-n>', function() harpoon_ui.nav_next() end)
         vim.keymap.set('n', '<C-p>', function() harpoon_ui.nav_prev() end)
