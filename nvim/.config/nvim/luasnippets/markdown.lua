e41c2f39d0c968e3b37cb76b1c8819b2241af8dc
Alessio Raviola <alessio.raviola.98@gmail.com>
2024-02-11 11:31:21 +0100

    Fix snip

---
+++ nvim/.config/nvim/luasnippets/markdown.lua
@@ -0,0 +1,20 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
+
+local markdown = {
+    s("ln", {
+        t"[", i(1), t"](",
+        f(function(name)
+            return string.lower(name[1][1]):gsub(" ", "-")
+        end, {1}),
+        t")"
+    })
+}
+
+ls.add_snippets("mdx", markdown)
+ls.add_snippets("md", markdown)
