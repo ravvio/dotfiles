774dd29e9b0cececfea3f2a41629cca2eadb0a70
Alessio Raviola <alessio.raviola.98@gmail.com>
2024-05-21 19:48:23 +0200

    obsidian nvim and zellij try

---
+++ nvim/.config/nvim/lua/graudo/plugins/obsidian-nvim.lua
@@ -0,0 +1,30 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  event = {
+    "BufReadPre " .. vim.fn.expand "~" .. "/vaults/**.md",
+    "BufNewFile " .. vim.fn.expand "~" .. "/vaults/**.md",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/personal",
+      },
+      {
+        name = "work",
+        path = "~/vaults/work",
+      },
+    },
+
+    notes_subdir = "notes",
+
+    daily_notes = {
+        folder = "notes/dailies"
+    },
+  },
+}
