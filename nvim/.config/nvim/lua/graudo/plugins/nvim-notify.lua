4b31e1a224e75320c3cc28c5d40dc4ae53e8993f
Alessio Raviola <alessio.raviola@codeploy.it>
2024-06-25 11:46:02 +0200

    Improve notify config

--- nvim/.config/nvim/lua/graudo/plugins/nvim-notify.lua
+++ nvim/.config/nvim/lua/graudo/plugins/nvim-notify.lua
@@ -2,6 +2,12 @@ return {
     "rcarriga/nvim-notify",
     prioriy = 900,
     config = function ()
-        vim.notify = require("notify")
+        local noti = require("notify")
+        noti.setup({
+            render = "wrapped-compact",
+            timeout = 3000,
+            minumum_width = 30,
+        })
+        vim.notify = noti
     end
 }
