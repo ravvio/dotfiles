4be1177c090017211ac32f9b60401a652e6e4640
Alessio Raviola <alessio.raviola.98@gmail.com>
2024-12-08 22:35:40 +0100

    editorconfig

--- nvim/.config/nvim/lua/graudo/extra/git.lua
+++ nvim/.config/nvim/lua/graudo/extra/git.lua
@@ -1,18 +1,18 @@
 -- Fast amend
 vim.api.nvim_create_user_command(
     "Amend",
-    function (opts)
+    function(opts)
         local bufname = vim.api.nvim_buf_get_name(0)
         local Job = require("plenary.job")
 
         Job:new({
             command = "git",
-            args = {'add', bufname}
+            args = { 'add', bufname }
         }):sync()
 
         Job:new({
             command = "git",
-            args = {"commit", "--amend", "--no-edit"},
+            args = { "commit", "--amend", "--no-edit" },
             on_exit = function()
                 local s = vim.split(bufname, "[/\\]")
                 vim.notify("Amended " .. s[#s])
